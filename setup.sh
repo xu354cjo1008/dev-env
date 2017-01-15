15c6ea9a152c5194cf10f3243f58b4038741f44d
ChingChang Li <xu354cjo1008@gmail.com>
2017-01-15 14:26:23 +0800

    Modify folder name in container to match host foler name

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 docker build -t="chingchangli/developer" .
-docker run -v /home/chingchangli/Developer:/home/chingchangli/working -t -i chingchangli/developer /bin/zsh
+docker run -v /home/chingchangli/Developer:/home/chingchangli/Developer -t -i chingchangli/developer /bin/zsh
